a4b247b175fea086deb41f6c31f31c5e2d14bcdb
José de Almeida <joshetava@gmai.com>
2023-09-27 15:32:45 -0300

    Estudo próprio - Inserindo contador de curtidas

---
+++ js/index.js
@@ -0,0 +1,10 @@
+const counter = document.querySelector('#counterValue');
+            /*const buttonDecrement = document.querySelector('#buttonDecrement');*/
+            const buttonIncrement = document.querySelector('#buttonIncrement');
+        
+        let  value = counter.value;
+        
+        buttonIncrement.addEventListener('click',() => {
+          value = ++value;
+          counter.value = value;
+        });
