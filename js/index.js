680547fc5983042f36b7dad00df8d4bf3d523db4
José de Almeida <joshetava@gmai.com>
2023-09-28 11:44:13 -0300

    Correção e Teste da div ContainerB

--- js/index.js
+++ js/index.js
@@ -2,9 +2,9 @@ const counter = document.querySelector('#counterValue');
             /*const buttonDecrement = document.querySelector('#buttonDecrement');*/
             const buttonIncrement = document.querySelector('#buttonIncrement');
         
-        let  value = counter.value;
+        value = counter.value;
         
         buttonIncrement.addEventListener('click',() => {
           value = ++value;
-          counter.value = value;
+          counter.value = ('click',(value));
         });
