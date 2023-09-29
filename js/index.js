31ecaa74f7eb5bc3797d44cba8d65ca483eceaa2
José de Almeida <joshetava@gmai.com>
2023-09-29 14:31:19 -0300

    Simulando curtidas

--- js/index.js
+++ js/index.js
@@ -1,11 +1,10 @@
 const counter = document.querySelector('#counterValue');
 /*const buttonDecrement = document.querySelector('#buttonDecrement');*/
 const buttonIncrement = document.querySelector('#buttonIncrement');
-
-let  value = counter.value;
-
-buttonIncrement.addEventListener('click',() => {
-  value = ++value;
-  value = 0;
-  counter.value = value;
-});
+        
+        value = counter.value;
+        
+        buttonIncrement.addEventListener('click',() => {
+          value = ++value;
+          counter.value = ('click',(value));
+        });
