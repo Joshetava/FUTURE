fe0d1c172e22a7f92f32083f498f5661382981bd
José de Almeida <joshetava@gmai.com>
2023-09-29 14:21:32 -0300

    Verificando erro no arquivo index.js

--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
 const counter = document.querySelector('#counterValue');
-            /*const buttonDecrement = document.querySelector('#buttonDecrement');*/
-            const buttonIncrement = document.querySelector('#buttonIncrement');
-        
-        value = counter.value;
-        
-        buttonIncrement.addEventListener('click',() => {
-          value = ++value;
-          counter.value = ('click',(value));
-        });
+/*const buttonDecrement = document.querySelector('#buttonDecrement');*/
+const buttonIncrement = document.querySelector('#buttonIncrement');
+
+let  value = counter.value;
+
+buttonIncrement.addEventListener('click',() => {
+  value = ++value;
+  value = 0;
+  counter.value = value;
+});
